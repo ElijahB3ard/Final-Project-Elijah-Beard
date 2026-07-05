3f3441cbbaae350478b5f0346b1bf2660d9c53f9
Elijah Beard <elijah.beard@proton.me>
2026-07-05 00:04:02 -0600

    Started task.hpp -> class Tasks, Tasks initializeTask

--- location.hpp
+++ location.hpp
@@ -12,13 +12,23 @@ class Location
         vector<NPCs> npcs;
         int trash;
     public:
+    // Setters
         void setName(string n)
         {
             name = n;
         }
+        void setTrash(int t)
+        {
+            trash = t;
+        }
 
+    // Getters - Not verrified
         string getName()
         {
             return name;
+        }
+        int getTrash()
+        {
+            return trash;
         }
 };
--- tasks.hpp
+++ tasks.hpp
@@ -4,7 +4,70 @@ using namespace std;
 class Tasks
 {
     private:
-        string name, required_equipment, location_reward;
+        string description, required_equipment, location_reward;
         int time, attribute_reward;
         Items item_reward;
+    public:
+    // setters - unverrified
+        void setDescription(string d)
+        {
+        description = d;
+        }
+        void setRequiredItem(string r)
+        {
+            required_equipment = r;
+        }
+        void setLocationReward(string l)
+        {
+            location_reward = l;
+        }
+        void setTime(int t)
+        {
+            time = t;
+        }
+        void setAttributeReward(int a)
+        {
+            attribute_reward = a;
+        }
+        void setItemReward(Items i)
+        {
+            item_reward = i;
+        }
+
+    // getters
+        string getDescription()
+        {
+            return description;
+        }
+        string getRequiredItem()
+        {
+            return required_equipment;
+        }
+        string getLocationReward()
+        {
+            return location_reward;
+        }
+        int getTime()
+        {
+            return time;
+        }
+        int getAttributeReward()
+        {
+            return attribute_reward;
+        }
+        Items getItemReward()
+        {
+            return item_reward;
+        }
 };
+
+Tasks initializeTask(string d, string re, string lr, int t, int ar, Items ir)
+{
+    Tasks task;
+
+    task.setDescription(d), task.setRequiredItem(re), task.setLocationReward(lr);
+    task.setTime(t), task.setAttributeReward(ar);
+    task.setItemReward(ir);
+
+    return task;
+}
